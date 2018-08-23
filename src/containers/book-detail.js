6a70a29b71728c5bf15f0f081d42251bc8a444c9
Bhanu Bhakta Sigdel <bsbhanu169@gmail.com>
2018-08-22 22:36:30 -0600

    Simple redux example

---
+++ src/containers/book-detail.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    if(!this.props.book) {
+      return (
+        <div>
+          Select a book to get started.
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <h3>Book detail for:</h3>
+        <div>Title: {this.props.book.title}</div>
+        <div>Pages: {this.props.book.pages}</div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  book: state.activeBook
+});
+
+export default connect(mapStateToProps)(BookDetail);
---
+++ src/reducers/reducer-books.js
@@ -0,0 +1,10 @@
+const reducerBooks = () => (
+  [
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 39 },
+    { title: 'The dark tower', pages: 85 },
+    { title: 'Eloquent Ruby', pages: 10 }
+  ]
+);
+
+export default reducerBooks;
